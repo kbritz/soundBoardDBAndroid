a75dbfba39271604a251d7c523be0b890413c609
kamilabrito <kamilabrito@192.168.10.7>
2014-03-03 10:41:55 -0400

    adding license and copyrights

--- soundBoardDBAndroid/src/com/br/kbrzt/soundboarddbandroid/constants.java
+++ soundBoardDBAndroid/src/com/br/kbrzt/soundboarddbandroid/constants.java
@@ -1,3 +1,22 @@
+/*DragonBall SoundBoard is a soundboard that plays some of dragon ball's 
+ famous characters lines in brazilian portuguese
+  
+    Copyright (C) 2014 Kamila Brito - kbrtzdevelopment - kbrtz.com
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package com.br.kbrzt.soundboarddbandroid;
 
 public class constants {
